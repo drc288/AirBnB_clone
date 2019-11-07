77662669632b84931e59a48a9168ff49cef6ce45
valerc123 <valentina.rua123@gmail.com>
2019-11-07 22:06:18 +0000

    basemodel

---
+++ datetime_.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from datetime import date
+from datetime import time
+from datetime import datetime
+from datetime import timedelta
+
+def main():
+    year = timedelta(days=365)
+    print(year)
+    
+
+if __name__== "__main__":
+    main()
---
+++ models/base_model.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+from datetime import datetime
+from uuid import uuid4
+
+"""
+    Module - BaseModel
+"""
+class BaseModel():
+
+    def __init__(self, *args, **kwargs):
+
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(__class__.__name__, self.id, self.__dict__)
+
+    def to_dict(self):
+        self.__dict__["created_at"] = self.created_at.isoformat()
+        self.__dict__["updated_at"] = self.updated_at.isoformat()
+        self.__dict__["__class__"] = __class__.__name__
+        return self.__dict__
+
+    def save(self):
+        self.updated_at = datetime.now()
+
