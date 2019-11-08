3fce13c9c97c544194da4962e436c8c5317dd4fc
Drose <davidroserocalle@gmail.com>
2019-11-08 16:18:15 +0000

    add point 4, i am a good dev >:3

--- models/base_model.py
+++ models/base_model.py
@@ -9,14 +9,31 @@ BaseModel - create the base object
 
 class BaseModel():
     """ BaseModel """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         __init__ construct the class, initialice the
          id, created at and updated at.
+         kwargs - get a new dict and converte the data a
+         BaseModel
         """
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = kwargs['id']
+                elif key == "created_at":
+                    # Convert to string a datetime format, using
+                    # datetime.strptime([date in str], ["the format of the string"])
+                    self.created_at = datetime.strptime(kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.updated_at = datetime.strptime(kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    # Add others values, create in object the keys if not are id, updated_at
+                    # and created_at, create the data using self.__dict__
+                    self.__dict__[key] = value
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
@@ -36,8 +53,8 @@ class BaseModel():
         """
         to_dict - return a string to a dict of the BaseModel.
         """
+        # Create the isoformat for datetime.now()
         self.__dict__["created_at"] = self.created_at.isoformat()
         self.__dict__["updated_at"] = self.updated_at.isoformat()
         self.__dict__['__class__'] = __class__.__name__
         return self.__dict__
-
