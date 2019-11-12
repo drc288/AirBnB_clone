69e074495b4e9cc5fde68417a04ad34eac544e5c
Drose <davidroserocalle@gmail.com>
2019-11-12 14:53:03 +0000

    pycode

---
+++ console.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import cmd
+"""
+Class prube - create command to print a interpreter
+"""
+
+
+class HBNBCommand(cmd.Cmd):
+    def do_quit(self, line):
+        """Quit command to exit the program
+        """
+        # Exit the command line
+        return True
+
+    def do_EOF(self, line):
+        """Quit program
+        """
+        # Exit program
+        return True
+
+
+if __name__ == '__main__':
+    # Run a infinithe loop
+    HBNBCommand().cmdloop()
