d6faa4c74c5c5c379dc9c0908fa8202dded7219f
Drose <davidroserocalle@gmail.com>
2019-11-12 16:45:17 +0000

    update and add console

--- console.py
+++ console.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
+from models.base_model import BaseModel
 import cmd
 """
-Class prube - create command to print a interpreter
+HBNBCommand - create command to print a interpreter
+for AirBnB
 """
+base = {'BaseModel': BaseModel}
 
 
 class HBNBCommand(cmd.Cmd):
+
     # Create a prompt hbnb
     prompt = '(hbnb) '
     def do_quit(self, line):
@@ -14,11 +18,37 @@ class HBNBCommand(cmd.Cmd):
         # Exit the command line
         return True
 
+    def emptyline(self):
+        pass
+
     def do_EOF(self, line):
-        """Quit program
+        """Quit command to exit the program
         """
         # Exit program
         return True
+
+    def do_create(self, line):
+        try:
+            if base[line]:
+                bs = base[line]()
+                bs.save()
+                print(bs.id)
+        except KeyError:
+            if line is "":
+                print("** class name missing **")
+            else:
+                print("** class doesn't exist **")
+
+    def do_show(self, line):
+        try:
+            for data in line:
+                print(data)
+        except:
+            pass
+
+
+
+
 
 
 if __name__ == '__main__':
