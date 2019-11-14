20d3ba596e2d3234a96c9d9ddf9a91c286a118d9
Drose <davidroserocalle@gmail.com>
2019-11-14 00:04:30 +0000

    fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 from models.base_model import BaseModel
 import json
 """
